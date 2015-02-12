3a19a910c87fc37943700d57744dd58cf8e38719
shape87 <gpetrochenkov@gmail.com>
2015-02-12 02:27:23 -0500

    Digi Assignment 2

---
+++ Nokolas Caige Dog Avoider/js/main.js
@@ -0,0 +1,176 @@
+window.onload = function() {
+    // You might want to start with a template that uses GameStates:
+    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
+    
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    
+    "use strict";
+    
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    
+    function preload() {
+        // Load an image and call it 'logo'.
+        game.load.image( 'nic', 'assets/200nic.png', 32 , 32 );
+        game.load.image( 'dog', 'assets/dog.png');
+        game.load.image( 'background', 'assets/park2.jpg' );
+    }
+    
+    var nic;
+    var dog;
+    var background;
+    var cursors;
+    var upKey;
+    var downKey;
+    var leftKey;
+    var rightKey;
+    var regPosition;
+    var run;
+    
+    function create() {
+        run = 5;
+        //game.physics.startSystem(Phaser.Physics.P2JS);
+       // game.physics.p2.defaultRestitution = 0.8;
+        // Create a sprite at the center of the screen using the 'logo' image.
+        background = game.add.sprite( '0', '0', 'background');
+        nic = game.add.sprite( game.world.centerX, 600, 'nic' );
+        dog = game.add.sprite( 0, 0, 'dog' );
+        //nic.body.setZeroDamping();
+        //nic.body.fixedRotation = true;
+        // Anchor the sprite at its center, as opposed to its top-left corner.
+        // so it will be truly centered.
+        nic.anchor.setTo( 0.5, 0.5 );
+        
+        // Turn on the arcade physics engine for this sprite.
+       
+        // Make it bounce off of the world bounds.
+        
+        game.physics.enable(nic, Phaser.Physics.ARCADE);
+        game.physics.enable(dog, Phaser.Physics.ARCADE);
+        nic.body.collideWorldBounds = true;
+        dog.body.collideWorldBounds = true;
+        //dog.body.immovable = true;
+        dog.body.velocity.setTo(200, 200);
+        dog.body.bounce.setTo(1, 1);
+       
+        // Add some text using a CSS style.
+        // Center it in X, and position its top 15 pixels from the top of the world.
+        
+       
+
+        upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+        regPosition = true;
+
+    }
+    
+
+    function update() {
+        // Accelerate the 'logo' sprite towards the cursor,
+        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
+        // in X or Y.
+        // This function returns the rotation angle that makes it visually match its
+        // new trajectory.
+       //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        if (upKey.isDown)
+        {
+            nic.y -= run;
+        }
+        else if (downKey.isDown)
+        {
+            nic.y += run;
+        }
+
+        if (leftKey.isDown)
+        {
+            nic.x -= run;
+        }
+        else if (rightKey.isDown)
+        {
+            nic.x += run;
+        }
+        // if (cursors.left.isDown)
+        // {
+        //     nic.body.moveLeft(400);
+        // }
+        // else if (cursors.right.isDown)
+        // {
+        //     nic.body.moveRight(400);
+        // }
+
+        // if (cursors.up.isDown)
+        // {
+        //     nic.body.moveUp(400);
+        // }
+        // else if (cursors.down.isDown)
+        // {
+        //     nic.body.moveDown(400);
+        // }
+
+        game.physics.arcade.collide(nic, dog, collisionHandler, null, this);
+        
+        
+
+        if(regPosition == true)
+        {
+             if(dog.x > 500)
+             {
+                run += 3;
+                dog.body.acceleration.x -= 600;
+                dog.body.acceleration.y -= run;
+                game.add.tween(dog.scale).to( { x: -1, y: 1 }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+                regPosition = !regPosition;
+             }
+        }
+        else
+        {
+             if(dog.x < 300)
+             {
+                run += 3;
+                dog.body.acceleration.x += 600;
+                dog.body.acceleration.y += run;
+                game.add.tween(dog.scale).to( { x: -1, y: 1 }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+                regPosition = !regPosition;
+                
+             }
+        }
+       
+    }
+
+    
+    function collisionHandler (obj1, obj2) {
+
+        game.add.tween(nic.scale).to( { x: .005, y: .005 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+        //nic.body.enable = false;
+        var style = { font: "50px arial", fill: "red", align: "center"};
+        var text = game.add.text( game.world.centerX, game.world.centerY - 100, "You Lose!!!", style );
+        var text2 = game.add.text( game.world.centerX, game.world.centerY, "Press refresh page to try again...", style );
+        
+        text.anchor.setTo( 0.5, 0.0 );
+        text2.anchor.setTo( 0.5, 0.0 );
+        
+
+    }
+
+    function render() {
+
+        game.debug.bodyInfo(dog, 32, 32);
+
+        game.debug.body(dog);
+       
+
+    }
+
+
+
+
+};
--- js/main.js
+++ js/main.js
@@ -16,15 +16,22 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'nic', 'assets/200nic.png', 32 , 32 );
-        game.load.image( 'dog', 'assets/dog.png');
-        game.load.image( 'background', 'assets/park2.jpg' );
+     
+        game.load.image( 'person', 'assets/person.png', 32 , 32 );
+        game.load.image( 'background', 'assets/background2.png' );
+        game.load.image('small_heart', 'assets/small_heart.png');
+        game.load.image('medium_heart', 'assets/medium_heart.png');
+        game.load.image('big_heart', 'assets/big_heart.png');
+        game.load.image('counter_back', 'assets/counter_back.png');
+        game.load.audio('music', ['assets/Avoidance.mp3', 'assets/Avoidance.ogg']);
+        
+        
+        
     }
     
-    var nic;
-    var dog;
+    var person;
     var background;
+    var counter_back;
     var cursors;
     var upKey;
     var downKey;
@@ -32,140 +39,421 @@ window.onload = function() {
     var rightKey;
     var regPosition;
     var run;
+    var duration = 0;
+    var jumpButton;
+    var jumpTimer = 0;
+    var counterText;
+    var counter = 0;
+    var heartCounter = 0;
+    var gameOver = false;
+    var line;
+    var heartList = [];
+    var smallHeartBool = false;
+    var medHeartBool = false;
+    var bigHeartBool = false;
+    var music;
+    var jump;
     
     function create() {
         run = 5;
-        //game.physics.startSystem(Phaser.Physics.P2JS);
-       // game.physics.p2.defaultRestitution = 0.8;
-        // Create a sprite at the center of the screen using the 'logo' image.
-        background = game.add.sprite( '0', '0', 'background');
-        nic = game.add.sprite( game.world.centerX, 600, 'nic' );
-        dog = game.add.sprite( 0, 0, 'dog' );
-        //nic.body.setZeroDamping();
-        //nic.body.fixedRotation = true;
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        nic.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
        
-        // Make it bounce off of the world bounds.
-        
-        game.physics.enable(nic, Phaser.Physics.ARCADE);
-        game.physics.enable(dog, Phaser.Physics.ARCADE);
-        nic.body.collideWorldBounds = true;
-        dog.body.collideWorldBounds = true;
-        //dog.body.immovable = true;
-        dog.body.velocity.setTo(200, 200);
-        dog.body.bounce.setTo(1, 1);
+        background = game.add.sprite( '0', '0', 'background');
+        counter_back = game.add.sprite( 50, 50, 'counter_back');
+        person = game.add.sprite( game.world.centerX, 600, 'person' );
+
+        music = game.add.audio('music');
+        music.play();
+       
+        game.physics.enable(person, Phaser.Physics.ARCADE);
+        person.body.collideWorldBounds = true;
        
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        
-       
-
+        person.col
         upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
         downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
         leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-
         regPosition = true;
+        duration = 5;
+
+        //
+        counterText = game.add.text(55,58, 'Counter: 0', { font: "40px Arial", fill: "red", align: "center" });
+        game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
+       
+        person.body.allowGravity = false;
+       
+        
+       
+       // counterText.anchor.setTo(0.5, 0.5);
+
+    }
+
+    function updateCounter()
+    {
+        if(!gameOver)
+        {
+            counter++;
+            heartCounter++;
+            counterText.setText('Counter: ' + counter);
+        }
+    }
+
+    function shootHearts()
+    {
+    
+        
+        if(heartCounter == 0 && !smallHeartBool)
+        {
+            var smallHeart = game.add.sprite(game.rnd.integerInRange(0, 800), 0, 'small_heart');
+            game.physics.enable(smallHeart, Phaser.Physics.ARCADE);
+            smallHeart.body.collideWorldBounds = true;
+            smallHeart.body.gravity.y = 1000
+            smallHeart.body.velocity.setTo(game.rnd.integerInRange(100, 200),game.rnd.integerInRange(100, 200));
+            smallHeart.body.bounce.setTo(1,1);
+            heartList.push(smallHeart);
+            smallHeartBool = true;
+            bigHeartBool = false;
+        }
+        else if(heartCounter == 7 && !medHeartBool)
+        {
+            var medHeart = game.add.sprite(game.rnd.integerInRange(0, 800), 0, 'medium_heart');
+            game.physics.enable(medHeart, Phaser.Physics.ARCADE);
+            medHeart.body.collideWorldBounds = true;
+            medHeart.body.gravity.y = 1000
+            medHeart.body.velocity.setTo(game.rnd.integerInRange(100, 200),game.rnd.integerInRange(100, 200));
+            medHeart.body.bounce.setTo(1,1);
+            heartList.push(medHeart);
+            medHeartBool = true;
+        }
+        else if(heartCounter == 15 && !bigHeartBool)
+        {
+            var bigHeart = game.add.sprite(game.rnd.integerInRange(0, 800), 0, 'big_heart');
+            game.physics.enable(bigHeart, Phaser.Physics.ARCADE);
+            bigHeart.body.collideWorldBounds = true;
+            bigHeart.body.gravity.y = 1000
+            bigHeart.body.velocity.setTo(game.rnd.integerInRange(100, 200),game.rnd.integerInRange(100, 200));
+            bigHeart.body.bounce.setTo(1,1);
+            heartList.push(bigHeart);
+
+            bigHeartBool = true;
+            smallHeartBool = false;
+            medHeartBool = false;
+            heartCounter = -1;
+        }
+        
 
     }
     
 
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-       //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+       shootHearts();
+
+       for( var i = 0; i < heartList.length; i++)
+       {
+         game.physics.arcade.collide(person, heartList[i], collisionHandler, null, this);
+       }
+         
+        customBounds(person);
+        if(counter % 38 == 0)
+        {
+            music.restart();
+        }
+
+       
         if (upKey.isDown)
         {
-            nic.y -= run;
-        }
-        else if (downKey.isDown)
-        {
-            nic.y += run;
+           person.y -= run;
         }
 
         if (leftKey.isDown)
         {
-            nic.x -= run;
+            if(regPosition)
+            {
+                person.scale.x = -1;
+                person.x += 45;
+                regPosition = !regPosition;
+            }
+            person.x -= run;
+        
+            
+            person.y += run;
+            
         }
         else if (rightKey.isDown)
         {
-            nic.x += run;
-        }
-        // if (cursors.left.isDown)
-        // {
-        //     nic.body.moveLeft(400);
-        // }
-        // else if (cursors.right.isDown)
-        // {
-        //     nic.body.moveRight(400);
-        // }
-
-        // if (cursors.up.isDown)
-        // {
-        //     nic.body.moveUp(400);
-        // }
-        // else if (cursors.down.isDown)
-        // {
-        //     nic.body.moveDown(400);
-        // }
-
-        game.physics.arcade.collide(nic, dog, collisionHandler, null, this);
-        
-        
-
-        if(regPosition == true)
-        {
-             if(dog.x > 500)
-             {
-                run += 3;
-                dog.body.acceleration.x -= 600;
-                dog.body.acceleration.y -= run;
-                //game.add.tween(dog.scale).to( { x: -1, y: 1 }, 50, Phaser.Easing.Linear.None, true, 0, 0, false);
+            if(!regPosition)
+            {
+                person.scale.x = 1;
+                person.x -= 45;
                 regPosition = !regPosition;
-             }
+            }
+            person.x += run;
+            
+            person.y += run;
+            
         }
-        else
+
+      
+        
+       
+    }
+
+    function customBounds()
+    {
+        if((person.x >= 0 && person.x <= 50) && person.y >= 350)
         {
-             if(dog.x < 300)
-             {
-                run += 3;
-                dog.body.acceleration.x += 600;
-                dog.body.acceleration.y += run;
-                //game.add.tween(dog.scale).to( { x: -1, y: 1 }, 50, Phaser.Easing.Linear.None, true, 0, 0, false);
-                regPosition = !regPosition;
-                
-             }
+           if(person.y > 350)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+        }
+        else if ((person.x >= 51 && person.x <= 100))
+        {
+            
+            if(person.y > 375)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 101 && person.x <= 150))
+        {
+            if(person.y > 400)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 151 && person.x <= 200))
+        {
+            if(person.y > 425)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 201 && person.x <= 250))
+        {
+           if(person.y > 425)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 251 && person.x <= 300))
+        {
+           if(person.y > 440)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 301 && person.x <= 350))
+        {
+           if(person.y > 440)
+            {
+               if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 351 && person.x <= 400))
+        {
+           if(person.y > 430)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        if((person.x >= 401 && person.x <= 450) && person.y >= 350)
+        {
+          if(person.y > 420)
+            {
+               if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+        }
+        else if ((person.x >= 451 && person.x <= 500))
+        {
+            if(person.y > 400)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 501 && person.x <= 550))
+        {
+            if(person.y > 390)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 551 && person.x <= 600))
+        {
+            if(person.y > 375)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 601 && person.x <= 650))
+        {
+            if(person.y > 345)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 651 && person.x <= 700))
+        {
+            if(person.y > 330)
+            {
+               if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 701 && person.x <= 750))
+        {
+            if(person.y > 310)
+            {
+                if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
+        }
+        else if ((person.x >= 751 && person.x <= 800))
+        {
+            if(person.y > 290)
+            {
+               if(jump)
+                {
+                    person.body.allowGravity = false;
+                }
+                jump = false;
+              
+                person.y -= 10;
+            }
+            
         }
        
     }
 
-    
+    function timeHandler ()
+    {
+
+    }
     function collisionHandler (obj1, obj2) {
 
-        game.add.tween(nic.scale).to( { x: .005, y: .005 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
-        //nic.body.enable = false;
-        var style = { font: "50px arial", fill: "red", align: "center"};
-        var text = game.add.text( game.world.centerX, game.world.centerY - 100, "You Lose!!!", style );
-        var text2 = game.add.text( game.world.centerX, game.world.centerY, "Press refresh page to try again...", style );
+        gameOver = true;
+        counterText.destroy();
+        counter_back.destroy();
+        game.add.tween(person.scale).to( { x: .005, y: .005 }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+        person.body.enable = false;
+        var style = { font: "25px arial", fill: "red", align: "center"};
+        var text = game.add.text( 50, game.world.centerY - 75, "Game Over!!!", style );
+        var text2 = game.add.text( 50, game.world.centerY - 25, "You lasted " + counter + " seconds!", style );
+        var text3 = game.add.text( 50, game.world.centerY + 25, "Press refresh page to try again...", style );
         
-        text.anchor.setTo( 0.5, 0.0 );
-        text2.anchor.setTo( 0.5, 0.0 );
+        // text.anchor.setTo( 0.5, 0.0 );
+        // text2.anchor.setTo( 0.5, 0.0 );
         
 
     }
 
     function render() {
 
-        game.debug.bodyInfo(dog, 32, 32);
+        // game.debug.bodyInfo(dog, 32, 32);
 
-        game.debug.body(dog);
+        // game.debug.body(dog);
        
 
     }
