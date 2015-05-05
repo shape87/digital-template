0d14c318ecc0837748e4d0c35ecb796006478d0a
Greg Petrochenkov <gpetrochenokv@gmail.com>
2015-05-04 20:48:17 -0400

    text

--- js/main.js
+++ js/main.js
@@ -15,7 +15,10 @@ window.onload = function() {
     
     var game = new Phaser.Game( 1000, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
-    function preload() {
+     function preload() {
+        
+        //level 1
+        game.load.spritesheet('bcMan2', 'assets/BCSpriteSheet2.png',150,189);
         game.load.image('background', 'assets/jungle_back.png');
         game.load.image('spikes', 'assets/death_spikes.png');
         game.load.image('small_plat', 'assets/small_plat.png');
@@ -27,6 +30,19 @@ window.onload = function() {
         game.load.spritesheet('flag', 'assets/flag.png');
         game.load.spritesheet('bat', 'assets/batsheet.png',200,85);
         game.load.spritesheet('spiral', 'assets/spiral.png');
+
+        //level 2
+        game.load.spritesheet('bcMan2', 'assets/BCSpriteSheet2.png',150,189);
+        game.load.image('background2', 'assets/background2.png');
+        game.load.image('lava', 'assets/lava.png');
+        game.load.image('lava2', 'assets/lava2.png');
+        game.load.image('lava3', 'assets/lava3.png');
+        game.load.image('platform', 'assets/platform.png');
+        game.load.audio('music2', ['assets/What Beats Lava-.mp3', 'assets/What Beats Lava-.ogg']);
+        game.load.spritesheet('finalPlatform', 'assets/finalplatform.png');
+
+        //level 3
+        // game.load.audio('music', ['assets/Craved In.mp3', 'assets/Craved In.ogg']);
     }
     
     var music;
@@ -49,6 +65,8 @@ window.onload = function() {
     var chaosCounter = 0;
     var gameOver = false;
     var style = { font: "40px arial", fill: "white", align: "center"};
+    var style2 = { font: "30px arial", fill: "white", align: "center"};
+    var text;
     var text;
     var textDuration = 0;
     var text2;
@@ -85,19 +103,21 @@ window.onload = function() {
     var smallPlats;
     var longPlats;
     var endPlat;
+    var level;
+    var lava;
+    var lava2;
+    var lava3;
+    var resetMusic = true;
     
     function create() {
-        music = game.add.audio('music');
-        music.play("",0,1,true,true);
-       
-        background = game.add.sprite( '0', '0', 'background');
-       
+        
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
         resetKey = game.input.keyboard.addKey(Phaser.Keyboard.R);
 
-        createGame(true);
-        game.world.setBounds(0, 0, 3000, 1200);
+        level = 1;
+        createGame();
+       
         
         upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
         downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
@@ -109,9 +129,35 @@ window.onload = function() {
 
     }
 
-    function createGame(totalReset)
+    function createGame(reset = true)
     {
         
+       if(level == 1)
+       {
+         level1(reset);
+       }
+       else
+       {
+         level2(reset);
+       }
+      
+
+       
+    }
+
+    function level1(totalReset)
+    {
+        if(resetMusic)
+        {
+            music = game.add.audio('music');
+            music.play("",0,1,true,true);
+           
+            background = game.add.sprite( '0', '0', 'background');
+            game.world.setBounds(0, 0, 3000, 1200);
+            resetMusic = false;
+        }
+       
+
         largePlats = game.add.group();
         smallPlats = game.add.group();
         longPlats  = game.add.group();
@@ -133,53 +179,114 @@ window.onload = function() {
         spikes3 = game.add.sprite(-50,1130,'spikes');
 
         
-       
-        
         animation.anchor.setTo(0.5,0.5);
         game.camera.follow(animation);
-        // lava3 = game.add.sprite(0, 1050, 'lava');
-        // var lavaTween3 = game.add.tween(lava3);
-        // lavaTween3.to({ y: 1070}, 1500, Phaser.Easing.Linear.None, true, 0, 2000, true);
         
        
         animation.animations.add('split',[18,19,20,21,22,23,24,25,26,27],15,false);
         animation.animations.add('walk',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],15,true);
-    
-        //enable physics
+
         game.physics.enable([animation,spikes], Phaser.Physics.ARCADE);
        
-
-        //flag.allowGravity = true;
-       //flag.body.gravity.y = 300;
         animation.body.allowGravity = true;
         animation.body.collideWorldBounds = true;
         animation.body.gravity.y = 1000;
         run = 4;
 
-        spikes.body.immovable = true;
-       
-      //  endPlatform.body.immovable = true;
-        
+        spikes.body.immovable = true; 
         deathText = game.add.text(400,450, '',style);
-
-        
         regPosition = true;
         
         if(totalReset)
         {
-             counterText = game.add.text(50,150, 'Time: ' + timer, { font: "40px Arial", fill: "black", align: "center" });
-             counterText.fixedToCamera = true;
-             //platSpeed = 1000;
-             
+             counterText = game.add.text(50,50, 'Level ' + level + '    Lives ' + lives + '    Time: ' + timer, 
+                { font: "30px Arial", fill: "black", align: "center" });
+             counterText.fixedToCamera = true;  
         }
-        // platforms();
-
-       
     }
 
+    function level2(totalReset)
+    {
+        if(resetMusic)
+        {
+            music = game.add.audio('music2');
+            music.play("",0,1,true,true);
+           
+            background = game.add.sprite( '0', '0', 'background2');
+            game.world.setBounds(0, 0, 3900, 1200);
+            resetMusic = false;
+        }
+      
+        // //assets
+        lava = game.add.sprite(0, 1030, 'lava3');
+        var lavaTween = game.add.tween(lava);
+        lavaTween.to({ y: 1070}, 2000, Phaser.Easing.Linear.None, true, 0, 2000, true);
+        lava2 = game.add.sprite(0, 1040, 'lava2');
+        var lavaTween2 = game.add.tween(lava2);
+        lavaTween2.to({ y: 1070}, 1750, Phaser.Easing.Linear.None, true, 0, 2000, true);
+        platform1 = game.add.sprite(0, 1000, 'platform');
+        platform2 = game.add.sprite(900,1000, 'platform');
+        platform3 = game.add.sprite(1400, 1000, 'platform');
+        platform4 = game.add.sprite(2100,950, 'platform');
+        platform5 = game.add.sprite(2800, 825, 'platform');
+        flag = game.add.sprite(3800,890,'flag');
+        endPlatform = game.add.sprite(3700,1030,'finalPlatform');
+
+        batGroup = game.add.group();
+        spiralGroup = game.add.group();
+
+        bats();
+
+        animation = game.add.sprite(0,900,'bcMan2',5);
+   
+        animation.anchor.setTo(0.5,0.5);
+        game.camera.follow(animation);
+        lava3 = game.add.sprite(0, 1050, 'lava');
+        var lavaTween3 = game.add.tween(lava3);
+        lavaTween3.to({ y: 1070}, 1500, Phaser.Easing.Linear.None, true, 0, 2000, true);
+        
+       
+        animation.animations.add('burn',[18,19,20,21,22,23,24,25,26,27,28,29,30,31],15,false);
+        animation.animations.add('walk',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],15,true);
+    
+        //enable physics
+        game.physics.enable([animation,platform1,platform2,platform3,platform4,platform5,
+            endPlatform,lava,flag], Phaser.Physics.ARCADE);
+
+        animation.body.allowGravity = true;
+        animation.body.collideWorldBounds = true;
+        animation.body.gravity.y = 1000;
+        run = 4;
+
+        platform1.body.immovable = true;
+        platform2.body.immovable = true;
+        platform3.body.immovable = true;
+        platform4.body.immovable = true;
+        platform5.body.immovable = true;
+        
+        deathText = game.add.text(400,450, '',{ font: "40px Arial", fill: "black", align: "center" });
+       
+        spirals();
+      
+        regPosition = true;
+        
+        if(totalReset)
+        {
+             counterText = game.add.text(50,50, 'Level ' + level + '   Lives ' + lives + '   Time: ' + timer, 
+              { font: "30px Arial", fill: "black", align: "center" });
+             counterText.fixedToCamera = true;
+             platSpeed = 1000;
+             
+        }
+        platforms();
+       
+
+    }
+
+    //-----------------------------------------LEvel one platforms
     function makePlats()
     {
-         flag = game.add.sprite(2800,180,'flag');
+        flag = game.add.sprite(2800,180,'flag');
 
         var lnPlatPos = [180, 465, 1470, 565, 2800, 465];
         for (var i = 0; i < 3; i++)
@@ -192,6 +299,7 @@ window.onload = function() {
         
         var lPlat = largePlats.create(lPlatPos[0],lPlatPos[1],'large_plat',0);   
         game.physics.enable(lPlat, Phaser.Physics.ARCADE);
+        lPlat.body.setSize(420,59,0,10);
         lPlat.body.immovable = true;
 
         endPlat = game.add.sprite(lPlatPos[2],lPlatPos[3],'large_plat',0);
@@ -205,6 +313,7 @@ window.onload = function() {
         {
             var sPlat = smallPlats.create(sPlatPos[i*2],sPlatPos[(i*2)+1],'small_plat',0);
             game.physics.enable(sPlat, Phaser.Physics.ARCADE);
+            sPlat.body.setSize(194,59,0,10);
             sPlat.body.immovable = true;
             if(i != 1)
             {
@@ -219,10 +328,24 @@ window.onload = function() {
              
             }
         }
+    }
 
+    //----------------------------Level two platforms
+    function platforms()
+    {
+        
+          //  plat1Tween = game.add.tween(platform1);
+            plat2Tween = game.add.tween(platform2);
+            plat3Tween = game.add.tween(platform3);
+            plat4Tween = game.add.tween(platform4);
+            plat5Tween = game.add.tween(platform5);
 
-
-      
+           // plat1Tween.to({ x: 250 }, platSpeed, Phaser.Easing.Linear.None, true, 0, platSpeed, true);
+            plat2Tween.to({ x: 550}, platSpeed, Phaser.Easing.Linear.None, true, 0, platSpeed, true);
+            plat3Tween.to({ y: 800 }, platSpeed, Phaser.Easing.Linear.None, true, 0, platSpeed, true);
+            plat4Tween.to({ y: 750 }, platSpeed + 100, Phaser.Easing.Linear.None, true, 0, platSpeed, true);
+            plat5Tween.to({ y: 625 }, platSpeed, Phaser.Easing.Linear.None, true, 0, platSpeed, true);
+        
     }
 
     function updateCounter()
@@ -230,7 +353,7 @@ window.onload = function() {
         if(!deathBool && !win && !gameOver)
         {
             timer++;
-            counterText.setText('Time: ' + timer);
+            counterText.setText('Level ' + level + '   Lives ' + lives + '   Time: ' + timer);
         }
         if(lives < 1 && !gameOver)
         {
@@ -259,12 +382,10 @@ window.onload = function() {
                 
             }
         }
-
-
     }
 
 
-    platHandler()
+    function platHandler()
     {
         animation.body.v.setTo(0,0);
         jumpBool = false;
@@ -272,11 +393,21 @@ window.onload = function() {
 
     function bats()
     {
-        var pos = [500,450, 1700,250, 2100,500]
+        if (level == 1)
+        {
+            var pos = [500,450, 1700,250, 2100,500]
+        }
+        else if(level == 2)
+        {
+            var pos = [500,700, 1850,800, 2500,800]
+        }
+       
         for (var i = 0;i < 3; i++)
         {
             var c = batGroup.create(pos[i*2],pos[(i*2)+1],'bat',0);
+           
             game.physics.enable(c);
+            c.body.setSize(150, 65, 25, 10);
             c.animations.add('fly',[0,1,2,3,4,5],15,true);
             c.animations.play('fly');
             game.add.tween(c).to( {y: c.y - 250 }, 1500, Phaser.Easing.Linear.None, true, 0, 500, true);
@@ -290,11 +421,20 @@ window.onload = function() {
 
     function spirals()
     {
-        var pos = [1025,550, 1700,800, 2300,550]
+        if(level == 1)
+        {
+            var pos = [1025,550, 1700,800, 2300,550];
+        }
+        else if ( level == 2)
+        {
+             var pos = [3350,800, 1400,1000, 3150,750];
+        }
+       
         for (var i = 0;i < 3; i++)
         {
             var sp = spiralGroup.create(pos[i*2],pos[(i*2)+1],'spiral');
             game.physics.enable(sp);
+            sp.body.setSize(150, 150, 50, 50);
             sp.anchor.setTo(.5,.5);
            // game.physics.enable(sp);
             game.add.tween(sp.scale).to( { x: .9, y: .9 }, 500, Phaser.Easing.Linear.None, true, 0, 500, true);
@@ -344,26 +484,27 @@ window.onload = function() {
         }
     }
 
-    // function burnAnimation()
-    // {
-    //     burn = true;
-    //     if (!gameOver)
-    //     {
-    //          if(!deathBool)
-    //          {
-    //              animation.animations.play('burn',15,false,false);
-    //              animation.body.velocity.setTo(0,0);
-    //              deathBool = true;
-    //              lives--;
-    //              counter = 0;
-    //              if(lives < 1 && !win)
-    //              {
-    //                  endGame();
-    //              }
-    //          }
-    //     }
+    function burnAnimation()
+    {
+        burn = true;
+        if (!gameOver)
+        {
+             if(!deathBool)
+             {
+                 animation.animations.play('burn',15,false,false);
+                 animation.body.velocity.setTo(0,0);
+                 deathBool = true;
+                 lives--;
+                 counter = 0;
+                 if(lives < 1 && !win)
+                 {
+                     endGame();
+                 }
+             }
+        }
 
-    // }
+    }
+
     function endGame()
     {
        gameOver = true;
@@ -406,40 +547,88 @@ window.onload = function() {
     function endHandler()
     {
         deathText.position.x = game.camera.position.x - 100;
-        deathText.setText('You win congratulations!!!!!\nPress R to start a new game...');
-        win = true;
+        if (level == 1)
+        {
+             win = true;
+            deathText.setText('You reached the goal!!!\nPress R to continue...');
+        }
+        else
+        {
+             win = true;
+            deathText.setText('You win congratulations!!!!!\nPress R to start a new game...');
+        }
+        
+       
     }
 
   
     function resetGame()
     {
-        largePlats.destroy();
-        longPlats.destroy();
-        smallPlats.destroy();
-        spikes.destroy();
+        
+      
+       
+        if(level==1)
+        {
+            largePlats.destroy();
+            longPlats.destroy();
+            smallPlats.destroy();
+            spikes.destroy();
+        }
+        else
+        {
+            animation.destroy();
+            platform1.destroy();
+            platform2.destroy();
+            platform3.destroy();
+            platform4.destroy();
+            platform5.destroy();
+            endPlatform.destroy();
+        }
+         if(level == 2)
+            { burn = false;}
+        
+
+        if(win)
+        {
+              background.destroy();
+              music.destroy();
+              if(level == 1)
+              {
+                level++;
+              }
+              else
+              {
+                level = 1;
+              }
+        }
+
         animation.destroy();
         flag.destroy();
         batGroup.destroy();
         spiralGroup.destroy();
-       
         deathText.setText(' ');
         turnBool = false;
         deathBool = false;
-        timer = 30;
+       
+
+       
+
         if(gameOver || win)
         {
+            timer = 0;
             gameOver = false;
             if(!win)
             {
-            text.destroy();
-            text3.destroy();
-            
-            deathAnimation.destroy();
+                text.destroy();
+                text3.destroy();
+                
+                deathAnimation.destroy();
             }
             else
             {
                 win = false;
                 counterText.destroy();
+                resetMusic = true;
             }
             lives = 3;
             counter = 0;
@@ -448,21 +637,40 @@ window.onload = function() {
         }
         else
         {
+            win = false;
             createGame(false);
         }
         
     }
 
     function update() {
-
-        game.physics.arcade.collide(largePlats,animation,platHandler, null,this);
-        game.physics.arcade.collide(smallPlats,animation,platHandler, null,this);
-        game.physics.arcade.overlap(spikes,animation,splitHandler, null,this);
-        game.physics.arcade.overlap(animation, batGroup, batHandler, null, this);
-        game.physics.arcade.overlap(animation, spiralGroup, spiralHandler, null, this);
-        game.physics.arcade.overlap(animation, spikes, spikeHandler, null, this);
-        game.physics.arcade.overlap(animation, endPlat, endHandler, null, this);
+        if (level == 1)
+        {
+            game.physics.arcade.collide(largePlats,animation,platHandler, null,this);
+            game.physics.arcade.collide(smallPlats,animation,platHandler, null,this);
+            game.physics.arcade.overlap(spikes,animation,splitHandler, null,this);
+            game.physics.arcade.overlap(animation, batGroup, batHandler, null, this);
+            game.physics.arcade.overlap(animation, spiralGroup, spiralHandler, null, this);
+            game.physics.arcade.overlap(animation, spikes, spikeHandler, null, this);
+            game.physics.arcade.overlap(animation, endPlat, endHandler, null, this);
       
+        } 
+        else
+        {
+
+           game.physics.arcade.collide(animation, platform1, platHandler, null, this);
+           game.physics.arcade.collide(animation, platform2, platHandler, null, this);
+           game.physics.arcade.collide(animation, platform3, platHandler, null, this);
+           game.physics.arcade.collide(animation, platform4, platHandler, null, this);
+           game.physics.arcade.collide(animation, platform5, platHandler, null, this);
+           game.physics.arcade.collide(animation, endPlatform, endHandler, null, this);
+          
+           game.physics.arcade.overlap(animation, lava, burnAnimation, null, this);
+           game.physics.arcade.overlap(animation, batGroup, batHandler, null, this);
+           game.physics.arcade.overlap(animation, spiralGroup, spiralHandler, null, this);
+        }
+       
+       
 
         if(!gameOver && !deathBool && !win)
         {
@@ -473,11 +681,7 @@ window.onload = function() {
                animation.y -= run;
                
             }
-            //  if (downKey.isDown)
-            // {
-            //    animation.y += run;
-               
-            // }
+        
 
             if (leftKey.isDown)
             {
@@ -516,12 +720,15 @@ window.onload = function() {
           
         }
 
-        if(animation.y >= 500)
+        if(level == 1)
         {
-            if(!turnBool)
+            if(animation.y >= 500)
             {
-                turnBool = true;
-                game.add.tween(animation).to( {angle:'+180' }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
+                if(!turnBool)
+                {
+                    turnBool = true;
+                    game.add.tween(animation).to( {angle:'+180' }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
+                }
             }
         }
                  
